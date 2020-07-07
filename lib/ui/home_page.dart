d2b2984dc2a188f4e22a60fa5bd648b81d7e6b61
gugabala <gugabala@gmail.com>
2020-07-07 16:09:34 -0300

    Projeto Finalizado

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,6 +4,7 @@ import 'package:com/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,7 +18,7 @@ class _HomePageState extends State<HomePage> {
 
   Future<Map> _getGifis() async {
     http.Response response;
-    if (_search == null) {
+    if (_search == null || _search.isEmpty) {
       print("------search null ok");
       response = await http.get(
           "https://api.giphy.com/v1/gifs/trending?api_key=h1GBl4kHXBOqKc7fMcKdhGGLv9ZUI9n4&limit=20&rating=g");
@@ -118,8 +119,10 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           if (_search == null || index < snapshot.data["data"].length)
             return GestureDetector(
-              child: Image.network(
-                snapshot.data["data"][index]["images"]["fixed_width"]["url"],
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: snapshot.data["data"][index]["images"]["fixed_width"]
+                    ["url"],
                 height: 300,
                 fit: BoxFit.cover,
               ),
